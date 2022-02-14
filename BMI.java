cfb9bc7c492e6a565d56ccc5ded2e89e8049fbd3
CyrussT <24277152+CyrussT@users.noreply.github.com>
2022-02-14 22:22:49 +0800

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -6,7 +6,7 @@ public class BMI {
     public BMI(double weight, double height) {
         this.weight = weight * 0.45359237; //convert to kilograms
         this.height = height * 0.0254; //convert to meters
-        this.BMI = this.weight / this.height / this.height; //calculate BMI
+        this.updateBMI(); //calculate BMI
     }
 
     public double getWeight() {
@@ -15,6 +15,7 @@ public class BMI {
 
     public void setWeight(double weight) {
         this.weight = weight;
+        this.updateBMI();
     }
 
     public double getHeight() {
@@ -23,18 +24,24 @@ public class BMI {
 
     public void setHeight(double height) {
         this.height = height;
+        this.updateBMI();
     }
 
     public double getBMI() {
         return BMI;
     }
 
-    public String diagnoseBMI(double bmi) {
-        if (bmi < 18.5) {
+    private void updateBMI()
+    {
+        this.BMI = this.weight / this.height / this.height;
+    }
+
+    public String diagnoseBMI() {
+        if (this.BMI < 18.5) {
             return "Underweight";
-        } else if (bmi < 25) {
+        } else if (this.BMI < 25) {
             return "Normal";
-        } else if (bmi < 30) {
+        } else if (this.BMI < 30) {
             return "Overweight";
         } else {
             return "Obese";
